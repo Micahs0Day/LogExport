f658d8f4abb220c152b931f4246ffeea7e8f619a
Micah Funderburk <71029494+Micahs0Day@users.noreply.github.com>
2025-06-12 17:26:17 -0400

    feat: new tf logic

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,44 +18,88 @@ variable "project" {
 variable "allowed_ip" {
   description = "The external IP address allowed to access the storage account."
   type        = string
+  default     = ""
+}
+
+variable "resource_group_name" {
+  default = "<your-rg-name>"
+  type    = string
+
 }
 
 #---------------------------
-# Resource Group
+# Virtual Network /  Subnet / NSG
 #---------------------------
-resource "azurerm_resource_group" "main" {
-  name     = "${var.project}-rg"
-  location = var.location
-}
-
-#---------------------------
-# Virtual Network and Subnet
-#---------------------------
-resource "azurerm_virtual_network" "vnet" {
+resource "azurerm_virtual_network" "log-export-vnet" {
   name                = "${var.project}-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = azurerm_resource_group.main.name
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "compute-subnet"
-  resource_group_name  = azurerm_resource_group.main.name
+resource "azurerm_subnet" "log-export-subnet" {
+  name                 = "${var.project}-subnet"
+  resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "log-export-nsg" {
+  name                = "${var.project}-subnet-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+#---------------------------
+# Application Insights
+#---------------------------
+resource "azurerm_application_insights" "log-export-insights" {
+  name                = "${var.project}-insights"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  application_type    = "web"
+}
+
+#---------------------------
+# Key Vault
+#---------------------------
+resource "azurerm_key_vault" "example" {
+  name                     = "${var.project}-keyvault"
+  location                 = var.location
+  resource_group_name      = var.resource_group_name
+  tenant_id                = data.azurerm_client_config.current.tenant_id
+  sku_name                 = "standard"
+  purge_protection_enabled = true
 }
 
 #---------------------------
 # Storage Account
 #---------------------------
-resource "azurerm_storage_account" "logstore" {
-  name                     = "${var.project}store${random_id.suffix.hex}"
-  resource_group_name      = azurerm_resource_group.main.name
+resource "azurerm_storage_account" "log-export-storage" {
+  name                     = "${var.project}-storage"
+  resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  allow_blob_public_access = false
-  min_tls_version          = "TLS1_2"
+
+  allow_nested_items_to_be_public = true
+  min_tls_version                 = "TLS1_2"
 
   network_rules {
     default_action             = "Deny"
@@ -84,14 +128,18 @@ resource "random_id" "suffix" {
 #---------------------------
 resource "azurerm_user_assigned_identity" "aml_identity" {
   name                = "${var.project}-aml-identity"
-  resource_group_name = azurerm_resource_group.main.name
+  resource_group_name = var.resource_group_name
   location            = var.location
 }
 
 resource "azurerm_machine_learning_workspace" "workspace" {
-  name                = "${var.project}-mlw"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.main.name
+  name                          = "${var.project}-mlw"
+  location                      = var.location
+  resource_group_name           = var.resource_group_name
+  public_network_access_enabled = false
+  application_insights_id       = ""
+  storage_account_id            = azurerm_storage_account.log-export-storage.id
+  key_vault_id                  = ""
 
   identity {
     type = "SystemAssigned"
@@ -100,12 +148,12 @@ resource "azurerm_machine_learning_workspace" "workspace" {
 }
 
 resource "azurerm_machine_learning_compute_instance" "compute" {
-  name                         = "log-export-compute"
-  location                     = var.location
-  resource_group_name          = azurerm_resource_group.main.name
+  name                          = "${var.project}-mlw-compute"
+  location                      = var.location
+  resource_group_name           = var.resource_group_name
   machine_learning_workspace_id = azurerm_machine_learning_workspace.workspace.id
-  virtual_machine_size         = "Standard_D8s_v3"
-  subnet_resource_id           = azurerm_subnet.subnet.id
+  virtual_machine_size          = "Standard_D8s_v3"
+  subnet_resource_id            = azurerm_subnet.subnet.id
 
   assign_to_user {
     object_id = "<YOUR_USER_OBJECT_ID>" # Replace with the user running jobs
@@ -113,7 +161,7 @@ resource "azurerm_machine_learning_compute_instance" "compute" {
   }
 
   identity {
-    type = "UserAssigned"
+    type         = "UserAssigned"
     identity_ids = [azurerm_user_assigned_identity.aml_identity.id]
   }
 }
